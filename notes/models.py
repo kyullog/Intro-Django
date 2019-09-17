7867080057c836b2de340a1e8c0f43a5f8e8fb89
Jonathan Dorety <kyullog@gmail.com>
2019-09-17 14:31:36 -0700

    Add datetime fields

--- notes/models.py
+++ notes/models.py
@@ -7,4 +7,8 @@ from uuid import uuid4
 class Note(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+
+
