a495bda7b73ca8e6988cf35fd32d743aea86a3f9
Jonathan Dorety <kyullog@gmail.com>
2019-09-23 20:54:41 -0700

    Creaet and implement PersonalNote

--- notes/admin.py
+++ notes/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
+# Register models here to have them show up in the admin view
 
+from django.contrib import admin
+from .models import Note, PersonalNote
 # Register your models here.
+
+
+class NoteAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'last_modified')
+
+
+admin.site.register((Note, PersonalNote), NoteAdmin)
